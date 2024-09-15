d7706c170647e251e792bb3852565a283eebe446
Gaurav Bansal <95044674+gbgaurav007@users.noreply.github.com>
2024-09-16 02:48:59 +0530

    Created Backend for fruit.ai

---
+++ backend/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from src.controllers.faq_controller import faq_blueprint
+from src.config.database import init_db, mongo
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+#CORS(app, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
+init_db(app)
+
+app.register_blueprint(faq_blueprint, url_prefix='/faqs')
+
+@app.route('/db-check', methods=['GET'])
+def db_check():
+    try:
+        mongo.db.faqs.find_one()
+        return jsonify({"message": "Successfully connected to MongoDB!"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
