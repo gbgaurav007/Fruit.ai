bdbd52d168423b89205ab49d86d75480adf8de46
Gaurav Bansal <95044674+gbgaurav007@users.noreply.github.com>
2024-09-16 14:44:10 +0530

    Updated Translator.jsx

--- frontend/src/components/Translator/Translator.jsx
+++ frontend/src/components/Translator/Translator.jsx
@@ -5,6 +5,7 @@ import IconControls from "./icon-controls";
 import ExchangeButton from "./ExchangeButton";
 import TranslateButton from "./TranslateButton";
 import { translateText } from "../../hooks/useTranslator";
+import { Link } from 'react-router-dom';
 
 function Translator() {
     const [fromText, setFromText] = useState("");
@@ -40,68 +41,74 @@ function Translator() {
     };
 
     return (
-        <div className="bg-gradient-to-tr from-sky-950 from-40% via-blue-900 via-75% to-blue-800 to-90%">
-        <div className="flex items-center justify-center min-h-screen p-5">
-            <div className="w-full bg-white p-6 shadow-lg rounded-lg max-w-xl">
-                <div className="flex flex-col sm:flex-row justify-between space-y-4 sm:space-y-0 sm:space-x-4">
-                    {/* Left Text Area (From Text) */}
-                    <div className="border border-gray-300 rounded-md w-full sm:w-1/2">
-                        <TextArea
+        <div className="bg-gradient-to-tr from-sky-950 from-40% via-blue-900 via-75% to-blue-800 to-90% flex flex-col">
+
+            <div className="absolute md:top-12 md:left-12 top-4 left-6 hover:scale-110">
+                <Link to="/home" className='flex flex-row'>
+                    <img
+                        src='back.png'
+                        alt="Back"
+                        className="md:w-10 md:h-9 w-8 h-7"
+                    />
+                </Link>
+            </div>
+
+            <div className="p-10 mt-20 font-dosis md:text-5xl text-4xl font-bold text-transparent bg-gradient-to-tr bg-clip-text from-yellow-300 via-rose-500 to-red-600">
+                Translator
+            </div>
+
+            <div className="flex items-center justify-center min-h-screen p-5">
+                <div className="w-full bg-white p-6 shadow-lg rounded-lg max-w-xl">
+                    <div className="flex flex-col sm:flex-row justify-between space-y-4 sm:space-y-0 sm:space-x-4">
+                        <div className="border border-gray-300 rounded-md w-full sm:w-1/2">
+                            <TextArea
                             value={fromText}
                             onChange={(e) => setFromText(e.target.value)}
                             placeholder="Enter text"
                             className="h-64 w-full resize-none border-none outline-none bg-transparent text-lg p-4"
                         />
-                    </div>
-                    
-                    {/* Right Text Area (To Text) */}
-                    <div className="border border-gray-300 rounded-md w-full sm:w-1/2">
-                        <TextArea
+                        </div>
+
+                        <div className="border border-gray-300 rounded-md w-full sm:w-1/2">
+                            <TextArea
                             value={toText}
                             readOnly
                             placeholder="Translation"
                             className="h-64 w-full resize-none border-none outline-none bg-transparent text-lg p-4"
                         />
+                        </div>
                     </div>
+
+                    <div className="flex justify-between p-4">
+                        <div className="flex flex-col sm:flex-row items-center sm:items-start sm:space-x-4 sm:mb-0">
+                            <div className="hidden sm:flex space-x-4 mt-2">
+                                <IconControls onCopy={handleCopy} onSpeak={handleSpeak} id="from" />
+                            </div>
+                            <div className="flex-1 overflow-x-scroll whitespace-nowrap">
+                                <LanguageSelect selectedLanguage={fromLang} onChange={(e) => setFromLang(e.target.value)} />
+                            </div>
+                        </div>
+
+                        <ExchangeButton onClick={handleExchange} />
+
+                        <div className="flex flex-col sm:flex-row items-center sm:items-start sm:space-x-4 sm:mb-0">
+                            <div className="flex-1 overflow-x-scroll whitespace-nowrap">
+                                <LanguageSelect selectedLanguage={toLang} onChange={(e) => setToLang(e.target.value)} />
+                            </div>
+                            <div className="hidden sm:flex space-x-4 mt-2">
+                                <IconControls onCopy={handleCopy} onSpeak={handleSpeak} id="to" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <button
+                        onClick={handleTranslate}
+                        className="w-full py-3 mt-5 text-white bg-blue-600 rounded-md"
+                    >
+                        Translate
+                    </button>
                 </div>
-
-                <div className="flex justify-between p-4">
-                    {/* Language Selectors and Icons (Left Side) */}
-                    <div className="flex flex-col sm:flex-row items-center sm:items-start sm:space-x-4 sm:mb-0">
-                        {/* Hide Icons on Mobile */}
-                        <div className="hidden sm:flex space-x-4 mt-2">
-                            <IconControls onCopy={handleCopy} onSpeak={handleSpeak} id="from" />
-                        </div>
-                        {/* Scrollable Language Select on Mobile */}
-                        <div className="flex-1 overflow-x-scroll whitespace-nowrap">
-                            <LanguageSelect selectedLanguage={fromLang} onChange={(e) => setFromLang(e.target.value)} />
-                        </div>
-                    </div>
-
-                    {/* Exchange Button */}
-                    <ExchangeButton onClick={handleExchange} />
-
-                    {/* Language Selectors and Icons (Right Side) */}
-                    <div className="flex flex-col sm:flex-row items-center sm:items-start sm:space-x-4 sm:mb-0">
-                        {/* Scrollable Language Select on Mobile */}
-                        <div className="flex-1 overflow-x-scroll whitespace-nowrap">
-                            <LanguageSelect selectedLanguage={toLang} onChange={(e) => setToLang(e.target.value)} />
-                        </div>
-                        {/* Hide Icons on Mobile */}
-                        <div className="hidden sm:flex space-x-4 mt-2">
-                            <IconControls onCopy={handleCopy} onSpeak={handleSpeak} id="to" />
-                        </div>
-                    </div>
-                </div>
-
-                <button
-                    onClick={handleTranslate}
-                    className="w-full py-3 mt-5 text-white bg-blue-600 rounded-md"
-                >
-                    Translate
-                </button>
             </div>
-        </div>
         </div>
     );
 }
